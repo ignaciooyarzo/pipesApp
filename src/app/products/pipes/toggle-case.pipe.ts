71e379f80f6b1624d5eaf9e22ef6354d46e352a3
Ignacio Oyarzo <ignaciooyarzo826@gmail.com>
2024-05-16 00:59:08 -0400

    finalizado

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class toggleCasePipe implements PipeTransform {
 
   transform(value: string, toUpper: boolean = false): string {
-    console.log({ value, toUpper });
+
     return ( toUpper )
      ? value.toUpperCase()
      : value.toLowerCase();
